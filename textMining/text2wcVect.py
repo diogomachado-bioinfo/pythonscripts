e19f13fff68be128ac533ff19e800c340c7c6c4c
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2019-11-25 20:15:54 -0300

    Update text2wcVect.py

--- textMining/text2wcVect.py
+++ textMining/text2wcVect.py
@@ -5,9 +5,9 @@ import numpy as np
 from sys import argv
 import pandas as pd
 from sklearn.feature_extraction import text
-my_stop_words = list(pd.read_csv('stopwords.txt',header=None)[0])
+#my_stop_words = list(pd.read_csv('stopwords.txt',header=None)[0])
 text = list(pd.read_csv(argv[1],sep='\t',header=None)[0].astype('U'))
-vectorizer = CountVectorizer(encoding='utf-8',stop_words=my_stop_words)
+vectorizer = CountVectorizer(encoding='utf-8',stop_words='english',lowercase=True)
 vectorizer.fit_transform(text)
 vector = vectorizer.transform(text)
 wl = vectorizer.get_feature_names()
