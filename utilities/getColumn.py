c1b6afe863e95d8f2bf2c7e8525932f62986fb3f
diogomachado-bioinfo <41521166+diogomachado-bioinfo@users.noreply.github.com>
2019-11-25 19:29:26 -0300

    Update getColumn.py

--- utilities/getColumn.py
+++ utilities/getColumn.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sys import argv
 import re
-import csv
 import pandas as pd
 X = np.array((pd.read_csv(argv[1],sep='\t',header=None)))
 idx=np.array(re.split(',',argv[2])).astype(int)
