d199c9a08e17033c5254c8f48507179e505697af
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2019-11-25 20:15:59 -0300

    Update text2tfidfVect.py

--- textMining/text2tfidfVect.py
+++ textMining/text2tfidfVect.py
@@ -6,7 +6,6 @@ from sys import argv
 import pandas as pd
 #my_stop_words = list(pd.read_csv('stopwords.txt',header=None)[0])
 text = list(pd.read_csv(argv[1],sep='\t',header=None)[0].astype('U'))
-#vectorizer = TfidfVectorizer(encoding='utf-8',stop_words='english',lowercase=True)
 vectorizer = TfidfVectorizer(encoding='utf-8',stop_words='english',lowercase=True)
 vectorizer.fit_transform(text)
 vector = vectorizer.transform(text)
